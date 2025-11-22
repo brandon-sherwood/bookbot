d2bc75620912619e33d28a60cbd03631f897ba8c
brandon-sherwood <sherwood.brandonlee@gmail.com>
2025-11-22 09:39:58 -0700

    Implemented word_count function

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ def get_book_text(path_to_file):
     return file_contents
 
 
+def word_count(file_contents):
+    text = file_contents.split()
+    num_words = len(text)
+    print(f"Found {num_words} total words")
+
+
 def main():
     frankenstein = get_book_text("books/frankenstein.txt")
-    print(frankenstein)
+    frankenstein_words = word_count(frankenstein)
 
 
 if __name__ == "__main__":
