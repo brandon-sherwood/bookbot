5044bfd1bae1c5b40bc0992d6222d4b0d172fa6e
brandon-sherwood <sherwood.brandonlee@gmail.com>
2025-11-23 11:06:46 -0700

    Implemented sorted_characters and sort_on in stats.py

--- stats.py
+++ stats.py
@@ -15,3 +15,19 @@ def character_count(file_contents):
             character[lower_char] = 1
 
     return character
+
+
+def sort_on(character):
+    return character["num"]
+
+
+def sorted_characters(character):
+    char_list = []
+
+    for i in character:
+        char_dict = {"char": i, "num": character[i]}
+        char_list.append(char_dict)
+
+    char_list.sort(reverse=True, key=sort_on)
+
+    return char_list
