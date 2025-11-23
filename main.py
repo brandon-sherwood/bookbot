0f3cb9fc415b2f9578103479d7690dfcd6443424
brandon-sherwood <sherwood.brandonlee@gmail.com>
2025-11-23 09:19:38 -0700

    Created stats.py file. Main calls word_count

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
+from stats import word_count
+
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
 
     return file_contents
-
-
-def word_count(file_contents):
-    text = file_contents.split()
-    num_words = len(text)
-    print(f"Found {num_words} total words")
 
 
 def main():
---
+++ stats.py
@@ -0,0 +1,4 @@
+def word_count(file_contents):
+    text = file_contents.split()
+    num_words = len(text)
+    print(f"Found {num_words} total words")
