3fb7f2d1e784e805291985d74d5949ed76bdcaad
brandon-sherwood <sherwood.brandonlee@gmail.com>
2025-11-23 11:34:36 -0700

    Implemented report print in main.py. Adjusted word_count to return instead of print in stats.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import word_count
 from stats import character_count
+from stats import sorted_characters
 
 
 def get_book_text(path_to_file):
@@ -13,7 +14,18 @@ def main():
     frankenstein = get_book_text("books/frankenstein.txt")
     frankenstein_words = word_count(frankenstein)
     frankenstein_characters = character_count(frankenstein)
-    print(frankenstein_characters)
+    frankenstein_sort_chars = sorted_characters(frankenstein_characters)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {frankenstein_words} total words")
+    print("--------- Character Count -------")
+    for i in frankenstein_sort_chars:
+        char = i["char"]
+        num = i["num"]
+        if char.isalpha():
+            print(f"{char}: {num}")
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -1,7 +1,7 @@
 def word_count(file_contents):
     text = file_contents.split()
     num_words = len(text)
-    print(f"Found {num_words} total words")
+    return num_words
 
 
 def character_count(file_contents):
